cff10557eefca05f3d0900885bb6f3fdc3e87b6b
datasci-rahul <69633561+datasci-rahul@users.noreply.github.com>
2022-11-25 18:49:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import sklearn as sk
+#import sklearn as sk
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier 
 import streamlit as st
